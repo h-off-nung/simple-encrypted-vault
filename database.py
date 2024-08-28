ccc2ff6da7f8f753258145c29b8f56da96273ef9
Kirill Bobrov <100440636+h-off-nung@users.noreply.github.com>
2024-08-28 13:59:34 +0200

    Refactor the database class to follow OOP

--- database.py
+++ database.py
@@ -1,41 +1,46 @@
 import sqlite3
 
-def connect_db():
-    return sqlite3.connect('jdbc:sqlite:bobrov_vault.db')
+class DatabaseManager:
+    def __init__(self, db_file='bobrov_vault.db'):
+        self.db_file = db_file
 
-def create_tables():
-    conn = connect_db()
-    cursor = conn.cursor()
+    def connect_db(self):
+        return sqlite3.connect(self.db_file)
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        master_password TEXT NOT NULL,
-        email TEXT NOT NULL
-    )
-    ''')
+    def create_tables(self):
+        conn = self.connect_db()
+        cursor = conn.cursor()
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS passwords (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        username TEXT NOT NULL,
-        password TEXT NOT NULL,
-        website TEXT
-    )
-    ''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            master_password TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
+        ''')
 
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT NOT NULL,
-        text TEXT NOT NULL
-    )
-    ''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS passwords (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            website TEXT
+        )
+        ''')
 
-    conn.commit()
-    conn.close()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS notes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            text TEXT NOT NULL
+        )
+        ''')
+
+        conn.commit()
+        conn.close()
 
 if __name__ == '__main__':
-    create_tables()
+    db_manager = DatabaseManager()
+    db_manager.create_tables()
